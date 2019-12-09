083aa38504b3f760ae54aa674021fd5d9f44c245
Joseph Benbella <josephbenbella@Josephs-MacBook-Air.local>
2019-12-09 09:45:26 -0700

    Get route for backlogged games is working

--- controllers/game.js
+++ controllers/game.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/users.js');
 const Game = require('../models/games.js');
 
+//Adds a new game item to a user's backlog list. 
 router.post('/', async (req, res) =>{
 	console.log(req.body)
 	try{
@@ -21,5 +22,21 @@ router.post('/', async (req, res) =>{
 		res.send(err);
 	}
 });
+
+//Finds all of the backlogged games for a user.
+router.get('/', async(req, res) =>{
+	try{
+		const foundUser = await User.findOne({'email': req.session.email})
+			.populate({
+				path: 'backlogGames',
+				match: { _id: req.params.id }
+			})
+			.exec()
+		res.send(foundUser);
+	} catch(err){
+		console.log(err);
+		res.send(err);
+	}
+}); 
 
 module.exports = router;
